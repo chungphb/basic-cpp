3952026fcb58ebe9a4cdfe73286620ef97e2e1b8
chungphb <phamhuubaochung@gmail.com>
2021-01-13 21:36:40 +0700

    Test STL algorithms: Partitioning, Other Permutation Algorithms

--- src/stl_algorithms_test.cpp
+++ src/stl_algorithms_test.cpp
@@ -2,6 +2,7 @@
 #include <set>
 #include <algorithm>
 #include <iostream>
+#include <random>
 
 void log(std::string_view txt, const std::vector<int>& vec) {
 	std::cout << txt << '\n';
@@ -78,8 +79,77 @@ void test_sorting() {
 	// Result: 1 2 3 4 5 6 7 8 9 10 11 12
 }
 
+// partition, partition_point
+void test_partition() {
+	std::cout << "\n[TEST PARTITION]\n";
+
+	std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+	log("Before partition: ", vec);
+	// Result: 1 2 3 4 5 6 7 8 9 10 11 12
+
+	std::partition(vec.begin(), vec.end(), [](const int& elm) {
+		return elm % 2;
+	});
+	log("After partition: ", vec);
+	// Result: 1 11 3 9 5 7 6 8 4 1 2 12
+
+	auto pp = std::partition_point(vec.begin(), vec.end(), [](const int& elm) {
+		return elm % 2;
+	});
+
+	std::cout << "First half: ";
+	std::copy(vec.begin(), pp, std::ostream_iterator<int>(std::cout, " "));
+	// Result: 1 11 3 9 5 7
+
+	std::cout << "\nSecond half: ";
+	std::copy(pp, vec.end(), std::ostream_iterator<int>(std::cout, " "));
+	std::cout << '\n';
+	// Result: 6 8 4 10 2 12
+}
+
+// reverse, rotate, shuffle, next_permutation, prev_permutation
+void test_other_permutation_algorithms() {
+	std::cout << "\n[TEST OTHER PERMUTATION ALGORITHMS]\n";
+
+	std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+	log("Before reverse: ", vec);
+	// Result: 1 2 3 4 5 6 7 8 9 10 11 12
+	
+	std::reverse(vec.begin(), vec.end());
+	log("After reverse: ", vec);
+	// Result: 12 11 10 9 8 7 6 5 4 3 2 1
+
+	std::rotate(vec.begin(), vec.begin() + vec.size() / 2, vec.end());
+	log("After rotate: ", vec);
+	// Result: 6 5 4 3 2 1 12 11 10 9 8 7
+
+	std::random_device rd;
+	std::default_random_engine dre(rd());
+	std::shuffle(vec.begin(), vec.end(), dre);
+	log("After shuffle: ", vec);
+	// Result: Anything possible
+
+	vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+	log("Before next_permutation: ", vec);
+	// Result: 1 2 3 4 5 6 7 8 9 10 11 12
+
+	std::next_permutation(vec.begin(), vec.end());
+	log("After next_permutation: ", vec);
+	// Result: 1 2 3 4 5 6 7 8 9 10 12 11
+
+	vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+	log("Before prev_permutation: ", vec);
+	// Result: 1 2 3 4 5 6 7 8 9 10 11 12
+
+	std::prev_permutation(vec.begin(), vec.end());
+	log("After prev_permutation: ", vec);
+	// Result: 12 11 10 9 8 7 6 5 4 3 2 1
+}
+
 int main(int argv, char** argc) {
 	test_heap();
 	test_sorting();
+	test_partition();
+	test_other_permutation_algorithms();
 }
 
