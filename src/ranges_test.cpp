7086f64154b6fe0908bd59584583709f0e63d151
chungphb <phamhuubaochung@gmail.com>
2021-01-19 23:59:23 +0700

    Test range-v3: Link range-v3 library

--- src/ranges_test.cpp
+++ src/ranges_test.cpp
@@ -1,10 +1,10 @@
-#include <ranges>
+#include <range/v3/all.hpp>
 #include <iostream>
 #include <vector>
 
 int main(int argc, char** argv) {
 	std::vector<int> numbers = {1, 2, 3, 4};
-	auto range = numbers | std::views::transform([](const int& number) {
+	auto range = numbers | ranges::views::transform([](int number) {
 		return number * 2;
 	});
 	std::copy(range.begin(), range.end(), std::ostream_iterator<int>(std::cout, " "));
