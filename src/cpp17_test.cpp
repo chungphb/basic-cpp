f058518b82dfd9226e190f44b8ce24b6b9271242
chungphb <phamhuubaochung@gmail.com>
2021-01-28 02:00:00 +0100

    Apply Boost.Test: cpp17_test.cpp

--- src/cpp17_test.cpp
+++ src/cpp17_test.cpp
@@ -1,7 +1,10 @@
+#define BOOST_TEST_MODULE cpp17 test
+#include <boost/test/included/unit_test.hpp>
+
 #include <iostream>
 #include <random>
 
-int main(int argc, char** argv) {
+BOOST_AUTO_TEST_CASE(test_sample) {
 	std::random_device rd;
 	std::mt19937 mt(rd());
 	std::uniform_int_distribution uid(0, 100);
