7bffa9403e989ce6708448ac1096013a00e40de5
chungphb <phamhuubaochung@gmail.com>
2021-01-30 02:37:16 +0700

    Apply Boost.Test: stl_algorithms_test.cpp

--- src/cpp17_test.cpp
+++ src/cpp17_test.cpp
@@ -3,11 +3,17 @@
 
 #include <iostream>
 #include <random>
+#include <vector>
 
-BOOST_AUTO_TEST_CASE(test_sample) {
+BOOST_AUTO_TEST_CASE(test_random) {
 	std::random_device rd;
 	std::mt19937 mt(rd());
 	std::uniform_int_distribution uid(0, 100);
 	auto r = uid(mt);
 	std::cout << "Hello " << r << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(test_vector) {
+	std::vector<int> vec{1, 2, 3, 4};
+	BOOST_TEST((vec == std::vector{1, 2, 3, 4}));
 }
