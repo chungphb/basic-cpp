e8e2367ff66e2384e3c050f3672c53335f4ef446
chungphb <phamhuubaochung@gmail.com>
2021-02-01 23:00:00 +0100

    Test Boost.Intrusive: Using member hooks

--- src/boost/boost_intrusive_test.cpp
+++ src/boost/boost_intrusive_test.cpp
@@ -11,7 +11,7 @@
 
 // 1. Using base hooks
 
-BOOST_AUTO_TEST_CASE(test_intrusive_containers_vs_non_intrusive_containers) {
+BOOST_AUTO_TEST_CASE(test_base_hook) {
 	TEST_MARKER();
 	
 	// Intrusive containers
@@ -49,5 +49,45 @@ BOOST_AUTO_TEST_CASE(test_multiple_base_hooks) {
 	fl2.push_back(f);
 
 	BOOST_CHECK(&f == &fl.front());
+	BOOST_CHECK(&f == &fl2.front());
+}
+
+// 2. Using member hooks
+
+BOOST_AUTO_TEST_CASE(test_member_hook) {
+	TEST_MARKER();
+	using namespace boost::intrusive;
+	struct foo {
+		list_member_hook<> hook;
+	};
+	using member_hook_option = member_hook<foo, list_member_hook<>, &foo::hook>;
+	using foo_list = list<foo, member_hook_option>;
+
+	foo f;
+	foo_list fl;
+	fl.push_back(f);
+
+	BOOST_CHECK(&f == &fl.front());
+}
+
+BOOST_AUTO_TEST_CASE(test_multiple_member_hooks) {
+	TEST_MARKER();
+	using namespace boost::intrusive;
+	struct foo {
+		list_member_hook<> hook_1;
+		list_member_hook<> hook_2;
+	};
+	using member_hook_option_1 = member_hook<foo, list_member_hook<>, &foo::hook_1>;
+	using foo_list_1 = list<foo, member_hook_option_1>;
+	using member_hook_option_2 = member_hook<foo, list_member_hook<>, &foo::hook_2>;
+	using foo_list_2 = list<foo, member_hook_option_2>;
+
+	foo f;
+	foo_list_1 fl1;
+	fl1.push_back(f);
+	foo_list_2 fl2;
+	fl2.push_back(f);
+
+	BOOST_CHECK(&f == &fl1.front());
 	BOOST_CHECK(&f == &fl2.front());
 }
