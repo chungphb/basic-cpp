4ce973b59d7d55fcce1c52655036cd3fc5fba70b
chungphb <phamhuubaochung@gmail.com>
2021-02-15 23:14:24 +0700

    Strong Types: Strongly typed constructors

--- src/stl/cpp17_test.cpp
+++ src/stl/cpp17_test.cpp
@@ -19,6 +19,7 @@ BOOST_AUTO_TEST_CASE(test_random) {
 }
 
 // TAG DISPATCHING
+// Link: https://www.fluentcpp.com/2018/04/27/tag-dispatching/
 
 BOOST_AUTO_TEST_CASE(test_tag_dispatching) {
 	TEST_MARKER();
@@ -38,4 +39,48 @@ BOOST_AUTO_TEST_CASE(test_tag_dispatching) {
 	foo f;
 	foo f_1{foo::tag_1{}};
 	foo f_2{foo::tag_2{}};
+}
+
+// EXPRESSIVE TYPES
+
+// Strongly typed constructors
+// Link: https://www.fluentcpp.com/2016/12/05/named-constructors/
+
+BOOST_AUTO_TEST_CASE(test_strongly_typed_constructors) {
+	TEST_MARKER();
+
+	struct expressive_type_1 {
+	public:
+		expressive_type_1(int v) : val{v} {}
+		int get() const {
+			return val;
+		}
+	private:
+		int val;
+	};
+
+	struct expressive_type_2 {
+	public:
+		expressive_type_2(int v) : val{v} {}
+		int get() const {
+			return val;
+		}
+	private:
+		int val;
+	};
+
+	struct foo {
+	public:
+		foo(expressive_type_1 v) : val{v.get()} {
+			std::cout << "Constructor 1\n";
+		}
+		foo(expressive_type_2 v) : val{v.get() + 1} {
+			std::cout << "Constructor 2\n";
+		}
+	private:
+		int val;
+	};
+
+	foo f_1{expressive_type_1{1}};
+	foo f_2{expressive_type_2{2}};
 }
