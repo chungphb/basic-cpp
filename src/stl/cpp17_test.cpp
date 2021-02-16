e943baec45781fc2852d10d5657bf2ac7e7b1ed3
chungphb <phamhuubaochung@gmail.com>
2021-02-16 21:49:45 +0700

    Strong Types: Implementing strong types

--- src/stl/cpp17_test.cpp
+++ src/stl/cpp17_test.cpp
@@ -84,3 +84,42 @@ BOOST_AUTO_TEST_CASE(test_strongly_typed_constructors) {
 	foo f_1{expressive_type_1{1}};
 	foo f_2{expressive_type_2{2}};
 }
+
+// Strongly types
+// Link: https://www.fluentcpp.com/2016/12/08/strong-types-for-strong-interfaces/
+
+namespace test_strong_types_ns {
+
+template <typename t, typename parameter>
+struct strong_type {
+public:
+	explicit strong_type(const t& v) : val{v} {}
+	explicit strong_type(t&& v) : val{std::move(v)} {}
+	t& get() {
+		return val;
+	}
+	const t& get() const {
+		return val;
+	}
+private:
+	t val;
+};
+
+}
+
+BOOST_AUTO_TEST_CASE(test_strong_types) {
+	TEST_MARKER();
+
+	using namespace test_strong_types_ns;
+	using strong_type_1 = strong_type<int, struct strong_type_1_parameter>;
+	using strong_type_2 = strong_type<int, struct strong_type_2_parameter>;
+
+	struct foo {
+	public:
+		foo(strong_type_1 v_1, strong_type_2 v_2) : val_1{v_1.get()}, val_2{v_2.get()} {}
+	private:
+		int val_1;
+		int val_2;
+	};
+	foo(strong_type_1{1}, strong_type_2{1});
+}
