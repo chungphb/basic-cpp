dcf5f38757953f6a44c4f70933b47aea00864de9
chungphb <phamhuubaochung@gmail.com>
2021-04-15 02:08:42 +0700

    Test concurrency: Test std::mutex, std::lock_guard

--- src/libs/stl/cpp11_concurrency_test.cpp
+++ src/libs/stl/cpp11_concurrency_test.cpp
@@ -1,8 +1,9 @@
 #define BOOST_TEST_MODULE cpp17 test
 #include <boost/test/included/unit_test.hpp>
 
-#include <thread>
 #include <iostream>
+#include <thread>
+#include <mutex>
 
 #include "test_util.h"
 
@@ -45,5 +46,108 @@ BOOST_AUTO_TEST_CASE(test_starting_multiple_threads_with_lambdas) {
 		thread.join();
 	}
 }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(test_mutex)
+
+BOOST_AUTO_TEST_CASE(test_protecting_shared_data_with_mutexes) {
+	TEST_MARKER();
+
+	struct foo {
+	public:
+		foo() : value{ 0 } {}
+		void increase() {
+			mutex.lock();
+			value++;
+			mutex.unlock();
+		}
+	public:
+		int value;
+		std::mutex mutex;
+	};
+
+	foo f;
+	const int NUM_THREADS = 4;
+	std::vector<std::thread> threads;
+	for (int i = 0; i < NUM_THREADS; i++) {
+		threads.push_back(std::thread{[&f] {
+			const int NUM_INCREMENTS = 100;
+			for (int i = 0; i < NUM_INCREMENTS; i++) {
+				f.increase();
+			}
+		}});
+	}
+	for (auto& thread : threads) {
+		thread.join();
+	}
+	std::cout << f.value << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(test_managing_locks_with_lock_guards) {
+	TEST_MARKER();
+
+	struct foo {
+	public:
+		foo() : value{0} {}
+		void increase() {
+			value++;
+		}
+		void decrease() {
+			if (value == 0) {
+				throw std::runtime_error("Value cannot be negative");
+			}
+			value--;
+		}
+	public:
+		int value;
+	};
+
+	struct concurrent_safe_foo {
+	public:
+		void increase() {
+			std::lock_guard<std::mutex> guard(mutex);
+			f.increase();
+		}
+		void decrease() {
+			std::lock_guard<std::mutex> guard(mutex);
+			f.decrease();
+		}
+		int value() {
+			return f.value;
+		}
+	public:
+		foo f;
+		std::mutex mutex;
+	};
+
+	concurrent_safe_foo f;
+	const int NUM_THREADS = 4;
+	std::vector<std::thread> threads;
+	for (int i = 0; i < NUM_THREADS; i++) {
+		threads.push_back(std::thread{[&f] {
+			const int NUM_INCREMENTS = 100;
+			for (int i = 0; i < NUM_INCREMENTS; i++) {
+				f.increase();
+			}
+		}});
+		threads.push_back(std::thread{[&f] {
+			const int NUM_DECREMENTS = 100;
+			for (int i = 0; i < NUM_DECREMENTS; i++) {
+				try {
+					f.decrease();
+				} catch (std::runtime_error& err) {
+					std::cout << "Error: " << err.what() << std::endl;
+				}
+				
+			}
+		}});
+	}
+	for (auto& thread : threads) {
+		thread.join();
+	}
+	std::cout << f.value() << std::endl;
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
