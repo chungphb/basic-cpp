77658d7acc300883c4ffdf052e77ea0f10fc2adb
chungphb <phamhuubaochung@gmail.com>
2022-08-24 19:35:23 +0700

    Two Pointers: Three Sum problem

--- src/sites/geeksforgeeks/algorithms/two_pointers_test.cpp
+++ src/sites/geeksforgeeks/algorithms/two_pointers_test.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_MODULE cpp17 test
 #include <boost/test/included/unit_test.hpp>
 #include <vector>
+#include <algorithm>
 
 #include "test_util.h"
 
@@ -14,9 +15,16 @@
 
 BOOST_AUTO_TEST_SUITE(test_two_pointers)
 
-void print(std::vector<int>& numbers) {
-	for (int number : numbers) {
-		std::cout << number << " ";
+void print(std::vector<int>& arr) {
+	for (int item : arr) {
+		std::cout << item << " ";
+	}
+	std::cout << "\n";
+}
+
+void print(std::vector<std::vector<int>>& arr2d) {
+	for (std::vector<int>& arr : arr2d) {
+		print(arr);
 	}
 	std::cout << "\n";
 }
@@ -68,6 +76,95 @@ BOOST_AUTO_TEST_CASE(two_sum_test) {
 		std::vector<int> arr = { -1, 0 };
 		int target = -1;
 		std::vector<int> res = twoSum(arr, target);
+		print(res);
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+// "Three Sum" problem (LeetCode #15)
+// Problem:
+// - Given an integer array A.
+// - Returns all the triplets A[i], A[j], and A[k] such that:
+//   + i != j, i != k, and j != k
+//   + A[i] + A[j] + A[k] == 0
+// 
+// Input: nums { -1 , 0, 1, 2, -1, -4 }
+// Output: {-1, -1, 2 }, { -1, 0, 1 }
+
+BOOST_AUTO_TEST_SUITE(three_sum_suite)
+
+std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+	// Sort the array
+	std::sort(nums.begin(), nums.end());
+
+	// The main loop
+	std::vector<std::vector<int>> res;
+	for (int i = 0; i < nums.size() - 2; ++i) {
+		// Forward the current pointer
+		if (i > 0 && nums[i] == nums[i - 1]) {
+			continue;
+		}
+
+		// The two pointers loop
+		int start = i + 1;
+		int end = nums.size() - 1;
+		while (start < end) {
+			int sum = nums[i] + nums[start] + nums[end];
+			if (sum == 0) {
+				res.push_back({nums[i], nums[start], nums[end]});
+
+				// Forward the start pointer
+				int tmp = nums[start];
+				do {
+					++start;
+				} while (start < end && nums[start] == tmp);
+
+				// Forward the end pointer
+				tmp = nums[end];
+				do {
+					--end;
+				} while (start < end && nums[end] == tmp);
+			} else if (sum > 0) {
+				--end;
+			} else {
+				++start;
+			}
+		}
+	}
+	return res;
+}
+
+BOOST_AUTO_TEST_CASE(three_sum_test) {
+	TEST_MARKER();
+
+	{ // Test 1
+		std::vector<int> arr = { -1, 0, 1, 2, -1, -4 };
+		auto res = threeSum(arr);
+		print(res);
+	}
+
+	{ // Test 2
+		std::vector<int> arr = { 0, 1, 1 };
+		auto res = threeSum(arr);
+		print(res);
+	}
+
+	{ // Test 3
+		std::vector<int> arr = { 0, 0, 0 };
+		auto res = threeSum(arr);
+		print(res);
+	}
+
+	{ // Test 4
+		std::vector<int> arr = { 0, 0, 0, 0 };
+		auto res = threeSum(arr);
+		print(res);
+	}
+
+	{ // Test 5
+		std::vector<int> arr = { -1, -1, 0, 0, 1, 1, 2, 2, -1, -1, -4, -4 };
+		auto res = threeSum(arr);
 		print(res);
 	}
 }
