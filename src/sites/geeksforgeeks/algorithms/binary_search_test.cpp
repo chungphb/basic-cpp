0394b47a562120ccd379ce78713c73f42c24810e
chungphb <phamhuubaochung@gmail.com>
2022-08-27 21:16:09 +0700

    Binary Search: Search Insert Position problem

--- src/sites/geeksforgeeks/algorithms/binary_search_test.cpp
+++ src/sites/geeksforgeeks/algorithms/binary_search_test.cpp
@@ -66,4 +66,71 @@ BOOST_AUTO_TEST_CASE(binary_search_test) {
 
 BOOST_AUTO_TEST_SUITE_END()
 
+// "Search Insert Position" problem (LeetCode #35)
+// Problem:
+// - Given a sorted array of distinct integers and a target value.
+// - Return the index if the target is found.
+// - If not, return the index where it would be if it were inserted in order.
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+
+BOOST_AUTO_TEST_SUITE(search_insert_position_suite)
+
+int searchInsert(std::vector<int>& nums, int target) {
+	int left = 0;
+	int right = nums.size() - 1;
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) {
+			return mid;
+		} else if (nums[mid] < target) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+	return left;
+}
+
+BOOST_AUTO_TEST_CASE(search_insert_position_test) {
+	TEST_MARKER();
+
+	{ // Test 1
+		std::vector<int> nums = { -1, 0, 3, 5, 9, 12 };
+		int target = 9;
+		int res = searchInsert(nums, target);
+		std::cout << res << "\n";
+	}
+
+	{ // Test 2
+		std::vector<int> nums = { -1, 0, 3, 5, 9, 12 };
+		int target = 2;
+		int res = searchInsert(nums, target);
+		std::cout << res << "\n";
+	}
+
+	{ // Test 3
+		std::vector<int> nums = { 1 };
+		int target = 1;
+		int res = searchInsert(nums, target);
+		std::cout << res << "\n";
+	}
+
+	{ // Test 4
+		std::vector<int> nums = { 1 };
+		int target = 2;
+		int res = searchInsert(nums, target);
+		std::cout << res << "\n";
+	}
+
+	{ // Test 5
+		std::vector<int> nums = { 2, 3, 4, 5 };
+		int target = 1;
+		int res = searchInsert(nums, target);
+		std::cout << res << "\n";
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE_END()
--- src/sites/geeksforgeeks/algorithms/dfs_test.cpp
+++ src/sites/geeksforgeeks/algorithms/dfs_test.cpp
@@ -589,6 +589,198 @@ BOOST_AUTO_TEST_CASE(convert_sorted_array_to_bst_test) {
 
 BOOST_AUTO_TEST_SUITE_END()
 
+
+// "Validate Binary Search Tree" problem (LeetCode #xxx)
+// Problem:
+// - Given the root of a binary tree.
+// - Determine if it is a valid binary search tree (BST).
+// Input: root = [5,1,4,null,null,3,6]
+// Output: false
+
+BOOST_AUTO_TEST_SUITE(validate_bst_suite)
+
+bool isValidBST(TreeNode* root) {
+	if (root == nullptr) {
+		return true;
+	}
+	TreeNode* node;
+
+	node = root->left;
+	while (node != nullptr) {
+		if (node->right == nullptr && node->val >= root->val) {
+			return false;
+		}
+		node = node->right;
+	}
+
+	node = root->right;
+	while (node != nullptr) {
+		if (node->left == nullptr && node->val <= root->val) {
+			return false;
+		}
+		node = node->left;
+	}
+
+	return isValidBST(root->left) && isValidBST(root->right);
+}
+
+BOOST_AUTO_TEST_CASE(validate_bst_test) {
+	TEST_MARKER();
+
+	{ // Test 1
+		TreeNode* root = new TreeNode(2);
+		root->left = new TreeNode(1);
+		root->right = new TreeNode(3);
+		std::cout << "Tree:\n";
+		print(root);
+		bool res = isValidBST(root);
+		std::cout << std::boolalpha << "Result: " << res << "\n\n";
+		release(root);
+	}
+
+	{ // Test 2
+		TreeNode* root = new TreeNode(5);
+		root->left = new TreeNode(1);
+		root->right = new TreeNode(4);
+		root->right->left = new TreeNode(3);
+		root->right->right = new TreeNode(6);
+		std::cout << "Tree:\n";
+		print(root);
+		bool res = isValidBST(root);
+		std::cout << std::boolalpha << "Result: " << res << "\n\n";
+		release(root);
+	}
+
+	{ // Test 3
+		TreeNode* root = new TreeNode(1);
+		std::cout << "Tree:\n";
+		print(root);
+		bool res = isValidBST(root);
+		std::cout << std::boolalpha << "Result: " << res << "\n\n";
+		release(root);
+	}
+
+	{ // Test 4
+		TreeNode* root = new TreeNode(10);
+		root->left = new TreeNode(5);
+		root->right = new TreeNode(15);
+		root->left->left = new TreeNode(3);
+		root->left->right = new TreeNode(7);
+		root->left->right->left = new TreeNode(6);
+		root->right->left = new TreeNode(12);
+		root->right->right = new TreeNode(17);
+		root->right->left->right = new TreeNode(14);
+		root->right->left->right->left = new TreeNode(13);
+		std::cout << "Tree:\n";
+		print(root);
+		bool res = isValidBST(root);
+		std::cout << std::boolalpha << "Result: " << res << "\n\n";
+		release(root);
+	}
+
+	{ // Test 5
+		TreeNode* root = new TreeNode(10);
+		root->left = new TreeNode(5);
+		root->right = new TreeNode(15);
+		root->left->left = new TreeNode(3);
+		root->left->right = new TreeNode(7);
+		root->left->right->left = new TreeNode(8);
+		root->right->left = new TreeNode(12);
+		root->right->right = new TreeNode(17);
+		root->right->left->right = new TreeNode(14);
+		root->right->left->right->left = new TreeNode(13);
+		std::cout << "Tree:\n";
+		print(root);
+		bool res = isValidBST(root);
+		std::cout << std::boolalpha << "Result: " << res << "\n\n";
+		release(root);
+	}
+
+	{ // Test 6
+		TreeNode* root = new TreeNode(2);
+		root->left = new TreeNode(2);
+		root->right = new TreeNode(2);
+		std::cout << "Tree:\n";
+		print(root);
+		bool res = isValidBST(root);
+		std::cout << std::boolalpha << "Result: " << res << "\n\n";
+		release(root);
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+// "Construct Binary Tree from Preorder and Inorder Traversal" problem (LeetCode #105)
+// Problem:
+// - Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree.
+// - Construct and return the binary tree.
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
+
+BOOST_AUTO_TEST_SUITE(construct_binary_tree_suite)
+
+TreeNode* dfs(int preStart, int inStart, int size, std::vector<int>& preorder, std::vector<int>& inorder) {
+	if (size == 0) {
+		return nullptr;
+	}
+
+	// Construct the root
+	int rootVal = preorder[preStart];
+	TreeNode* root = new TreeNode(rootVal);
+
+	// Determine the size of the subtrees
+	int leftSize = 0;
+	for (int k = inStart; k < inStart + size; ++k) {
+		if (inorder[k] == rootVal) {
+			break;
+		}
+		++leftSize;
+	}
+	int rightSize = size - leftSize - 1;
+
+	// Construct the subtrees
+	root->left = dfs(preStart + 1, inStart, leftSize, preorder, inorder);
+	root->right = dfs(preStart + leftSize + 1, inStart + leftSize + 1, rightSize, preorder, inorder);
+	return root;
+}
+
+TreeNode* buildTree(std::vector<int>& preorder, std::vector<int>& inorder) {
+	if (preorder.size() == 0) {
+		return nullptr;
+	}
+	return dfs(0, 0, preorder.size(), preorder, inorder);
+}
+
+BOOST_AUTO_TEST_CASE(construct_binary_tree_test) {
+	TEST_MARKER();
+
+	{ // Test 1
+		std::vector<int> preorder = { 3, 9, 20, 15, 7 };
+		std::vector<int> inorder = { 9, 3, 15, 20, 7 };
+		TreeNode* root = buildTree(preorder, inorder);
+		print(root);
+		release(root);
+	}
+
+	{ // Test 2
+		std::vector<int> preorder = { -1 };
+		std::vector<int> inorder = { -1 };
+		TreeNode* root = buildTree(preorder, inorder);
+		print(root);
+		release(root);
+	}
+
+	{ // Test 3
+		std::vector<int> preorder = { 10, 5, 3, 7, 6, 15, 12, 14, 13, 17 };
+		std::vector<int> inorder = { 3, 5, 6, 7, 10, 12, 13, 14, 15, 17 };
+		TreeNode* root = buildTree(preorder, inorder);
+		print(root);
+		release(root);
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
 // ========================================
 // COMBINATIONS, PERMUTATIONS, SUBSETS
 // ========================================
@@ -1013,7 +1205,7 @@ BOOST_AUTO_TEST_CASE(subsets_test) {
 BOOST_AUTO_TEST_SUITE_END()
 
 // ========================================
-// COMBINATIONS, PERMUTATIONS, SUBSETS
+// MATRIX
 // ========================================
 
 // "Word Search" problem (LeetCode #79)
@@ -1109,197 +1301,6 @@ BOOST_AUTO_TEST_CASE(word_search_test) {
 		std::string word = "ABCB";
 		bool res = exist(board, word);
 		std::cout << std::boolalpha << res << std::endl;
-	}
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-// "Validate Binary Search Tree" problem (LeetCode #xxx)
-// Problem:
-// - Given the root of a binary tree.
-// - Determine if it is a valid binary search tree (BST).
-// Input: root = [5,1,4,null,null,3,6]
-// Output: false
-
-BOOST_AUTO_TEST_SUITE(validate_bst_suite)
-
-bool isValidBST(TreeNode* root) {
-	if (root == nullptr) {
-		return true;
-	}
-	TreeNode* node;
-
-	node = root->left;
-	while (node != nullptr) {
-		if (node->right == nullptr && node->val >= root->val) {
-			return false;
-		}
-		node = node->right;
-	}
-
-	node = root->right;
-	while (node != nullptr) {
-		if (node->left == nullptr && node->val <= root->val) {
-			return false;
-		}
-		node = node->left;
-	}
-
-	return isValidBST(root->left) && isValidBST(root->right);
-}
-
-BOOST_AUTO_TEST_CASE(validate_bst_test) {
-	TEST_MARKER();
-
-	{ // Test 1
-		TreeNode* root = new TreeNode(2);
-		root->left = new TreeNode(1);
-		root->right = new TreeNode(3);
-		std::cout << "Tree:\n";
-		print(root);
-		bool res = isValidBST(root);
-		std::cout << std::boolalpha << "Result: " << res << "\n\n";
-		release(root);
-	}
-
-	{ // Test 2
-		TreeNode* root = new TreeNode(5);
-		root->left = new TreeNode(1);
-		root->right = new TreeNode(4);
-		root->right->left = new TreeNode(3);
-		root->right->right = new TreeNode(6);
-		std::cout << "Tree:\n";
-		print(root);
-		bool res = isValidBST(root);
-		std::cout << std::boolalpha << "Result: " << res << "\n\n";
-		release(root);
-	}
-
-	{ // Test 3
-		TreeNode* root = new TreeNode(1);
-		std::cout << "Tree:\n";
-		print(root);
-		bool res = isValidBST(root);
-		std::cout << std::boolalpha << "Result: " << res << "\n\n";
-		release(root);
-	}
-
-	{ // Test 4
-		TreeNode* root = new TreeNode(10);
-		root->left = new TreeNode(5);
-		root->right = new TreeNode(15);
-		root->left->left = new TreeNode(3);
-		root->left->right = new TreeNode(7);
-		root->left->right->left = new TreeNode(6);
-		root->right->left = new TreeNode(12);
-		root->right->right = new TreeNode(17);
-		root->right->left->right = new TreeNode(14);
-		root->right->left->right->left = new TreeNode(13);
-		std::cout << "Tree:\n";
-		print(root);
-		bool res = isValidBST(root);
-		std::cout << std::boolalpha << "Result: " << res << "\n\n";
-		release(root);
-	}
-
-	{ // Test 5
-		TreeNode* root = new TreeNode(10);
-		root->left = new TreeNode(5);
-		root->right = new TreeNode(15);
-		root->left->left = new TreeNode(3);
-		root->left->right = new TreeNode(7);
-		root->left->right->left = new TreeNode(8);
-		root->right->left = new TreeNode(12);
-		root->right->right = new TreeNode(17);
-		root->right->left->right = new TreeNode(14);
-		root->right->left->right->left = new TreeNode(13);
-		std::cout << "Tree:\n";
-		print(root);
-		bool res = isValidBST(root);
-		std::cout << std::boolalpha << "Result: " << res << "\n\n";
-		release(root);
-	}
-
-	{ // Test 6
-		TreeNode* root = new TreeNode(2);
-		root->left = new TreeNode(2);
-		root->right = new TreeNode(2);
-		std::cout << "Tree:\n";
-		print(root);
-		bool res = isValidBST(root);
-		std::cout << std::boolalpha << "Result: " << res << "\n\n";
-		release(root);
-	}
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-// "Construct Binary Tree from Preorder and Inorder Traversal" problem (LeetCode #105)
-// Problem:
-// - Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree.
-// - Construct and return the binary tree.
-// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
-// Output: [3,9,20,null,null,15,7]
-
-BOOST_AUTO_TEST_SUITE(construct_binary_tree_suite)
-
-TreeNode* dfs(int preStart, int inStart, int size, std::vector<int>& preorder, std::vector<int>& inorder) {
-	if (size == 0) {
-		return nullptr;
-	}
-
-	// Construct the root
-	int rootVal = preorder[preStart];
-	TreeNode* root = new TreeNode(rootVal);
-
-	// Determine the size of the subtrees
-	int leftSize = 0;
-	for (int k = inStart; k < inStart + size; ++k) {
-		if (inorder[k] == rootVal) { 
-			break;
-		}
-		++leftSize;
-	}
-	int rightSize = size - leftSize - 1;
-
-	// Construct the subtrees
-	root->left = dfs(preStart + 1, inStart, leftSize, preorder, inorder);
-	root->right = dfs(preStart + leftSize + 1, inStart + leftSize + 1, rightSize, preorder, inorder);
-	return root;
-}
-
-TreeNode* buildTree(std::vector<int>& preorder, std::vector<int>& inorder) {
-	if (preorder.size() == 0) {
-		return nullptr;
-	}
-	return dfs(0, 0, preorder.size(), preorder, inorder);
-}
-
-BOOST_AUTO_TEST_CASE(construct_binary_tree_test) {
-	TEST_MARKER();
-
-	{ // Test 1
-		std::vector<int> preorder = { 3, 9, 20, 15, 7 };
-		std::vector<int> inorder = { 9, 3, 15, 20, 7 };
-		TreeNode* root = buildTree(preorder, inorder);
-		print(root);
-		release(root);
-	}
-
-	{ // Test 2
-		std::vector<int> preorder = { -1 };
-		std::vector<int> inorder = { -1 };
-		TreeNode* root = buildTree(preorder, inorder);
-		print(root);
-		release(root);
-	}
-
-	{ // Test 3
-		std::vector<int> preorder = { 10, 5, 3, 7, 6, 15, 12, 14, 13, 17 };
-		std::vector<int> inorder = { 3, 5, 6, 7, 10, 12, 13, 14, 15, 17 };
-		TreeNode* root = buildTree(preorder, inorder);
-		print(root);
-		release(root);
 	}
 }
 
