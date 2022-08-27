46aad8c21b997d98934c682923a8c9eb5f302c77
chungphb <phamhuubaochung@gmail.com>
2022-08-27 20:46:56 +0700

    Binary Search: Binary Search problem

---
+++ src/sites/geeksforgeeks/algorithms/binary_search_test.cpp
@@ -0,0 +1,69 @@
+#define BOOST_TEST_MODULE cpp17 test
+#include <boost/test/included/unit_test.hpp>
+#include <vector>
+
+#include "test_util.h"
+
+BOOST_AUTO_TEST_SUITE(test_binary_search)
+
+// "Binary Search" problem (LeetCode #704)
+// Problem:
+// - Given an array of integers nums which is sorted in ascending order, and an integer target.
+// - Write a function to search target in nums.
+// - If target exists, then return its index. Otherwise, return -1.
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+
+BOOST_AUTO_TEST_SUITE(binary_search_suite)
+
+int search(std::vector<int>& nums, int target) {
+	int left = 0;
+	int right = nums.size() - 1;
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) {
+			return mid;
+		} else if (nums[mid] < target) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+	return -1;
+}
+
+BOOST_AUTO_TEST_CASE(binary_search_test) {
+	TEST_MARKER();
+
+	{ // Test 1
+		std::vector<int> nums = { -1, 0, 3, 5, 9, 12 };
+		int target = 9;
+		int res = search(nums, target);
+		std::cout << res << "\n";
+	}
+
+	{ // Test 2
+		std::vector<int> nums = { -1, 0, 3, 5, 9, 12 };
+		int target = 2;
+		int res = search(nums, target);
+		std::cout << res << "\n";
+	}
+
+	{ // Test 3
+		std::vector<int> nums = { 1 };
+		int target = 1;
+		int res = search(nums, target);
+		std::cout << res << "\n";
+	}
+
+	{ // Test 4
+		std::vector<int> nums = { 1 };
+		int target = 2;
+		int res = search(nums, target);
+		std::cout << res << "\n";
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
