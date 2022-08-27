7b9ba8b9d5e14a6292009531efedfb437fed7f17
chungphb <phamhuubaochung@gmail.com>
2022-08-27 21:42:41 +0700

    Binary Search: Search a 2D Matrix problem

--- src/sites/geeksforgeeks/algorithms/binary_search_test.cpp
+++ src/sites/geeksforgeeks/algorithms/binary_search_test.cpp
@@ -133,4 +133,82 @@ BOOST_AUTO_TEST_CASE(search_insert_position_test) {
 
 BOOST_AUTO_TEST_SUITE_END()
 
+// "Search a 2D Matrix" problem (LeetCode #74)
+// Problem:
+// - Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix.
+// - This matrix has the following properties:
+//   + Integers in each row are sorted from left to right.
+//   + The first integer of each row is greater than the last integer of the previous row.
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+
+BOOST_AUTO_TEST_SUITE(search_2d_matrix_suite)
+
+bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
+	int m = matrix.size();
+	int n = matrix[0].size();
+	int left = 0;
+	int right = m * n - 1;
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+		int midVal = matrix[mid / n][mid % n];
+		if (midVal == target) {
+			return true;
+		} else if (midVal < target) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+	return false;
+}
+
+BOOST_AUTO_TEST_CASE(search_2d_matrix_test) {
+	TEST_MARKER();
+
+	{ // Test 1
+		std::vector<std::vector<int>> matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+		int target = 3;
+		bool res = searchMatrix(matrix, target);
+		std::cout << std::boolalpha << res << std::endl;
+	}
+
+	{ // Test 2
+		std::vector<std::vector<int>> matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+		int target = 23;
+		bool res = searchMatrix(matrix, target);
+		std::cout << std::boolalpha << res << std::endl;
+	}
+
+	{ // Test 3
+		std::vector<std::vector<int>> matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+		int target = 22;
+		bool res = searchMatrix(matrix, target);
+		std::cout << std::boolalpha << res << std::endl;
+	}
+
+	{ // Test 4
+		std::vector<std::vector<int>> matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+		int target = 0;
+		bool res = searchMatrix(matrix, target);
+		std::cout << std::boolalpha << res << std::endl;
+	}
+
+	{ // Test 5
+		std::vector<std::vector<int>> matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+		int target = 100;
+		bool res = searchMatrix(matrix, target);
+		std::cout << std::boolalpha << res << std::endl;
+	}
+
+	{ // Test 6
+		std::vector<std::vector<int>> matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+		int target = 60;
+		bool res = searchMatrix(matrix, target);
+		std::cout << std::boolalpha << res << std::endl;
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE_END()
