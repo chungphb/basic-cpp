5cb52ee090d682ec5329f4898ddfd37e861b4556
chungphb <phamhuubaochung@gmail.com>
2022-08-30 03:40:40 +0700

    Priority Queue: Kth Largest Element in a Stream problem

--- src/sites/leetcode/data_structures/heap_test.cpp
+++ src/sites/leetcode/data_structures/heap_test.cpp
@@ -8,7 +8,7 @@
 
 #include "test_util.h"
 
-BOOST_AUTO_TEST_SUITE(test_hash_leetcode)
+BOOST_AUTO_TEST_SUITE(test_heap_leetcode)
 
 template <typename T>
 void print(std::vector<T>& arr) {
@@ -97,6 +97,61 @@ BOOST_AUTO_TEST_CASE(relative_ranks_test) {
 		std::cout << "Placement: ";
 		print(res);
 		std::cout << "\n";
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+// "Kth Largest Element in a Stream" problem (LeetCode #703)
+// Problem:
+// - Design a class to find the kth largest element in a stream.
+// - Note that it is the kth largest element in the sorted order, not the kth distinct element.
+// Input: ["KthLargest", "add", "add", "add", "add", "add"]
+// [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+// Output: [null, 4, 5, 5, 8, 8]
+
+BOOST_AUTO_TEST_SUITE(kth_largest_suite)
+
+class KthLargest {
+private:
+	int _k;
+	std::priority_queue<int, std::vector<int>, std::greater<int>> _heap;
+public:
+	KthLargest(int k, std::vector<int>& nums) {
+		_k = k;
+		for (int num : nums) {
+			_heap.push(num);
+		}
+		while (_heap.size() > _k) {
+			_heap.pop();
+		}
+	}
+
+	int add(int val) {
+		_heap.push(val);
+		if (_heap.size() > _k) {
+			_heap.pop();
+		}
+		return _heap.top();
+	}
+};
+
+BOOST_AUTO_TEST_CASE(kth_largest_test) {
+	TEST_MARKER();
+
+	{ // Test 1
+		int k = 3;
+		std::vector<int> nums = { 4, 5, 8, 2 };
+		std::cout << "Nums: ";
+		print(nums);
+		KthLargest* obj = new KthLargest(k, nums);
+		std::vector<int> vals = { 3, 5, 10, 9, 4, 3, 4, 9, 1, 7, 6, 3, 2, 5, 4, 1 };
+		for (int val : vals) {
+			int res = obj->add(val);
+			std::cout << "Add: " << val << ", Extract: " << res << "\n";
+		}
+		std::cout << "\n";
+		delete obj;
 	}
 }
 
