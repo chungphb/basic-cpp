d91e0b50cacbe3ef96e8b458670deb481e4179e1
chungphb <phamhuubaochung@gmail.com>
2022-08-30 10:12:03 +0700

    LeetCode: Rotate Image problem

--- src/sites/leetcode/random_test.cpp
+++ src/sites/leetcode/random_test.cpp
@@ -376,4 +376,106 @@ BOOST_AUTO_TEST_CASE(permutations_2_test) {
 
 BOOST_AUTO_TEST_SUITE_END()
 
+// "Rotate Image" problem (LeetCode #48)
+// Problem:
+// - Given an n x n 2D matrix representing an image.
+// - Rotate the image by 90 degrees (clockwise).
+// - You have to rotate the image in-place.
+// Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+// Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+BOOST_AUTO_TEST_SUITE(rotate_image_suite)
+
+void rotate(std::vector<std::vector<int>>& matrix) {
+	int n = matrix.size();
+	for (int circle = 0; circle < n / 2; ++circle) {
+		int low = circle;
+		int high = n - circle - 1;
+		for (int row = low; row < high; ++row) {
+			int tmp = matrix[row][low];
+			matrix[row][low] = matrix[high][row];
+			matrix[high][row] = matrix[(high + low) - row][high];
+			matrix[(high + low) - row][high] = matrix[low][(high + low) - row];
+			matrix[low][(high + low) - row] = tmp;
+		}
+	}
+}
+
+BOOST_AUTO_TEST_CASE(rotate_image_test) {
+	TEST_MARKER();
+
+	{ // Test 1
+		std::vector<std::vector<int>> matrix = {
+			{ 1, 2, 3 },
+			{ 4, 5, 6 },
+			{ 7, 8, 9 },
+		};
+		std::cout << "Before:\n";
+		print(matrix);
+		rotate(matrix);
+		std::cout << "After:\n";
+		print(matrix);
+		std::cout << "\n";
+	}
+
+	{ // Test 2
+		std::vector<std::vector<int>> matrix = {
+			{ 5, 1, 9, 11 },
+			{ 2, 4, 8, 10 },
+			{ 13, 3, 6, 7 },
+			{ 15, 14, 12, 16 }
+		};
+		std::cout << "Before:\n";
+		print(matrix);
+		rotate(matrix);
+		std::cout << "After:\n";
+		print(matrix);
+		std::cout << "\n";
+	}
+
+	{ // Test 3
+		std::vector<std::vector<int>> matrix = {
+			{ 1, 2 },
+			{ 3, 4 }
+		};
+		std::cout << "Before:\n";
+		print(matrix);
+		rotate(matrix);
+		std::cout << "After:\n";
+		print(matrix);
+		std::cout << "\n";
+	}
+
+	{ // Test 4
+		std::vector<std::vector<int>> matrix = {
+			{ 1 }
+		};
+		std::cout << "Before:\n";
+		print(matrix);
+		rotate(matrix);
+		std::cout << "After:\n";
+		print(matrix);
+		std::cout << "\n";
+	}
+
+	{ // Test 5
+		std::vector<std::vector<int>> matrix = {
+			{ 1, 2, 3, 4, 5, 6 },
+			{ 1, 2, 3, 4, 5, 6 },
+			{ 1, 2, 3, 4, 5, 6 },
+			{ 1, 2, 3, 4, 5, 6 },
+			{ 1, 2, 3, 4, 5, 6 },
+			{ 1, 2, 3, 4, 5, 6 }
+		};
+		std::cout << "Before:\n";
+		print(matrix);
+		rotate(matrix);
+		std::cout << "After:\n";
+		print(matrix);
+		std::cout << "\n";
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE_END()
