77761e821196755a58f641fabbba41de08e63a84
chungphb <phamhuubaochung@gmail.com>
2022-08-30 08:45:39 +0700

    LeetCode: Multiply Strings problem

--- src/sites/leetcode/random_test.cpp
+++ src/sites/leetcode/random_test.cpp
@@ -164,4 +164,122 @@ BOOST_AUTO_TEST_CASE(count_and_say_test) {
 
 BOOST_AUTO_TEST_SUITE_END()
 
+// "Multiply Strings" problem (LeetCode #43)
+// Problem:
+// - Given two non-negative integers num1 and num2 represented as strings.
+// - Return the product of num1 and num2, also represented as a string.
+// Input: num1 = "123", num2 = "456"
+// Output: "56088"
+
+BOOST_AUTO_TEST_SUITE(multiply_strings_suite)
+
+std::string fix(std::string num) {
+	std::string res;
+	bool ignore0 = true;
+	for (int i = num.size() - 1; i >= 0; --i) {
+		if (num[i] == '0' && ignore0) {
+			continue;
+		}
+		ignore0 = false;
+		res += num[i];
+	}
+	return res;
+}
+
+std::string multiply(std::string num1, std::string num2) {
+	if (num1 == "0" || num2 == "0") {
+		return "0";
+	}
+	int n1 = num1.size();
+	int n2 = num2.size();
+	int start = 0;
+	std::string num(n1 + n2 + 1, '0');
+	for (int k1 = n1 - 1; k1 >= 0; --k1) {
+		int c1 = num1[k1] - '0';
+		int k = start;
+		int offset = 0;
+		for (int k2 = n2 - 1; k2 >= 0; --k2) {
+			int c2 = num2[k2] - '0';
+			int sum = (num[k] - '0') + (c1 * c2) + offset;
+			num[k] = sum % 10 + '0';
+			offset = sum / 10;
+			++k;
+		}
+		if (offset > 0) {
+			num[k] = offset + '0';
+		}
+		++start;
+	}
+	return fix(num);
+}
+
+BOOST_AUTO_TEST_CASE(multiply_strings_test) {
+	TEST_MARKER();
+
+	{ // Test 1
+		std::string num1 = "2";
+		std::string num2 = "3";
+		std::string res = multiply(num1, num2);
+		std::cout << num1 << " x " << num2 << " = " << res << "\n";
+	}
+
+	{ // Test 2
+		std::string num1 = "2";
+		std::string num2 = "5";
+		std::string res = multiply(num1, num2);
+		std::cout << num1 << " x " << num2 << " = " << res << "\n";
+	}
+
+	{ // Test 3
+		std::string num1 = "123";
+		std::string num2 = "456";
+		std::string res = multiply(num1, num2);
+		std::cout << num1 << " x " << num2 << " = " << res << "\n";
+	}
+
+	{ // Test 4
+		std::string num1 = "99999";
+		std::string num2 = "99999";
+		std::string res = multiply(num1, num2);
+		std::cout << num1 << " x " << num2 << " = " << res << "\n";
+	}
+
+	{ // Test 5
+		std::string num1 = "99";
+		std::string num2 = "99999";
+		std::string res = multiply(num1, num2);
+		std::cout << num1 << " x " << num2 << " = " << res << "\n";
+	}
+
+	{ // Test 6
+		std::string num1 = "99999";
+		std::string num2 = "99";
+		std::string res = multiply(num1, num2);
+		std::cout << num1 << " x " << num2 << " = " << res << "\n";
+	}
+
+	{ // Test 7
+		std::string num1 = "0";
+		std::string num2 = "100";
+		std::string res = multiply(num1, num2);
+		std::cout << num1 << " x " << num2 << " = " << res << "\n";
+	}
+
+	{ // Test 8
+		std::string num1 = "1";
+		std::string num2 = "100";
+		std::string res = multiply(num1, num2);
+		std::cout << num1 << " x " << num2 << " = " << res << "\n";
+	}
+
+	{ // Test 9
+		std::string num1 = "9";
+		std::string num2 = "8";
+		std::string res = multiply(num1, num2);
+		std::cout << num1 << " x " << num2 << " = " << res << "\n";
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE_END()
