d681bcea75b459bee060dd5cc28ece77bfd583a0
chungphb <phamhuubaochung@gmail.com>
2022-09-04 23:23:17 +0700

    Design Patterns: Factory Method

---
+++ src/sites/refactoring_guru/creational/factory_method.cpp
@@ -0,0 +1,68 @@
+#define BOOST_TEST_MODULE cpp17 test
+#include <boost/test/included/unit_test.hpp>
+#include "test_util.h"
+
+BOOST_AUTO_TEST_SUITE(design_patterns_test)
+
+BOOST_AUTO_TEST_SUITE(factory_method_suite)
+
+class Product {
+public:
+	virtual ~Product() = default;
+	virtual std::string operation() const = 0;
+};
+
+class ConcreteProduct1 : public Product {
+public:
+	std::string operation() const override {
+		return "Product 1";
+	}
+};
+
+class ConcreteProduct2 : public Product {
+public:
+	std::string operation() const override {
+		return "Product 2";
+	}
+};
+
+class Creator {
+public:
+	virtual ~Creator() = default;
+	virtual Product* factoryMethod() const = 0;
+};
+
+class ConcreteCreator1 : public Creator {
+public:
+	Product* factoryMethod() const override {
+		return new ConcreteProduct1();
+	}
+};
+
+class ConcreteCreator2 : public Creator {
+public:
+	Product* factoryMethod() const override {
+		return new ConcreteProduct2();
+	}
+};
+
+void clientRequest(const Creator& creator) {
+	Product* product = creator.factoryMethod();
+	std::string result = product->operation();
+	std::cout << result << "\n";
+	delete product;
+}
+
+BOOST_AUTO_TEST_CASE(factory_method_test) {
+	TEST_MARKER();
+	Creator* creator1 = new ConcreteCreator1();
+	clientRequest(*creator1);
+	delete creator1;
+	Creator* creator2 = new ConcreteCreator2();
+	clientRequest(*creator2);
+	delete creator2;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
